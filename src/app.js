c158bd938f8654a1582da661245697b703cee869
Bryan Navarro <diazbnavarro@gmail.com>
2025-09-30 20:27:32 +0000

    adding function, not functional yet

--- src/app.js
+++ src/app.js
@@ -8,6 +8,13 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  function randomfigure() {
+    let figures = ['♦', '♥', '♠' ,'♣'];
+    figures = Math.floor(Math.random() * figures.length)
+    
+  }
+
 };
 
 
